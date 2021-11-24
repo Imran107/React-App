979e0f308fb5fd7779d84139bc752219db3079b4
Imran107 <imran.itim@gmail.com>
2021-11-24 20:57:20 +0500

    Adding state to counter component

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -2,15 +2,37 @@ import React, {Component} from 'react'
 import './Counter.css'
 
 class Counter extends Component{
+
+  //Define the initial state in a constructor
+  //state => counter 0
+  constructor() {
+    super(); //Error 1
+
+    this.state = {
+        counter : 0
+    }
+
+    this.increment = this.increment.bind(this);
+  }
+
   render(){
     return (
       <div className="App">
-        <CounterButton by={1}></CounterButton>
-        <CounterButton by={5}></CounterButton>
-        <CounterButton by={10}></CounterButton>
+        <CounterButton by={1} incrementMethod={this.increment}></CounterButton>
+        <CounterButton by={5} incrementMethod={this.increment}></CounterButton>
+        <CounterButton by={10} incrementMethod={this.increment}></CounterButton>
+        <span className="count" style={{color:"red"}}>{this.state.counter}</span>
       </div>
     );
   }
+
+  increment(by) { 
+    console.log(`increment from parent ${by}`);
+    this.setState({
+        counter: this.state.counter + by
+    });
+  }
+
 }
 
 class CounterButton extends Component {  
@@ -42,6 +64,8 @@ class CounterButton extends Component {
     this.setState({
         counter: this.state.counter + this.props.by
     });
+
+    this.props.incrementMethod(this.props.by)
   }
 
 }
