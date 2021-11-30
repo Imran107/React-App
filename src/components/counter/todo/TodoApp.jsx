df097440f73c2b06dc3fa140c643bc3a30d057d7
Imran107 <imran.itim@gmail.com>
2021-11-30 23:04:24 +0500

    Adding Login Componenent and hard-coded validation

--- src/components/counter/todo/TodoApp.jsx
+++ src/components/counter/todo/TodoApp.jsx
@@ -16,10 +16,13 @@ class LoginComponent extends Component{
         super(props);
         this.state = {
             username : "in28minutes",
-            password : ""
+            password : "",
+            hasLoginFailed : false,
+            showSuccessMessage : false
         }
 
         this.handleChange = this.handleChange.bind(this)
+        this.loginClicked = this.loginClicked.bind(this)
     }
 
     handleChange(event){
@@ -29,15 +32,41 @@ class LoginComponent extends Component{
         });
     }
 
+    loginClicked(){
+        if(this.state.username === 'in28minutes' && this.state.password ==='dummy'){
+            this.setState({hasLoginFailed : false})
+            this.setState({showSuccessMessage : true})
+        }else{
+            this.setState({hasLoginFailed : true})
+            this.setState({showSuccessMessage : false})
+        }
+    }
+
     render(){
         return(
             <div>
+                <ShowLoginFailedMessage hasLoginFailed={this.state.hasLoginFailed}/>
+                <ShowLoginSuccessMessage showSuccessMessage={this.state.showSuccessMessage}/>
                 User Name : <input type="text" name="username" value={this.state.username} onChange={this.handleChange}/>
                 Password : <input type="password" name="password" onChange={this.handleChange}/>
-                <button>Login</button>
+                <button onClick={this.loginClicked}>Login</button>
             </div>
         );
     }
+}
+
+function ShowLoginFailedMessage(props){
+    if(props.hasLoginFailed){
+        return <div> Invalid Credentials!</div>
+    }
+    return null;
+}
+
+function ShowLoginSuccessMessage(props){
+    if(props.showSuccessMessage){
+        return <div> Login Succeeded</div>
+    }
+    return null;
 }
 
 export default TodoApp
