1fccaf60d378afeeee9b031c3ce9664b420378cd
Imran107 <imran.itim@gmail.com>
2021-12-06 17:55:12 +0500

    Add redirect url to each link

--- src/components/counter/todo/TodoApp.jsx
+++ src/components/counter/todo/TodoApp.jsx
@@ -28,14 +28,14 @@ class HeaderComponent extends Component{
         return(
             <header>
                 <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-                    <div><a className="navbar-brand">in28minutes</a></div>
+                    <div><a href="http://www.in28minutes.com" className="navbar-brand">in28minutes</a></div>
                     <ul className="navbar-nav">
-                        <li className="nav-link">Home</li>
-                        <li className="nav-link">Todos</li>
+                        <li><Link className="nav-link" to="/welcome/in28minutes">Home</Link></li>
+                        <li><Link className="nav-link" to="/todos">Todos</Link></li>
                     </ul>
                     <ul className="navbar-nav navbar-collapse justify-content-end">
-                        <li className="nav-link">Login</li>
-                        <li className="nav-link">Logout</li>
+                        <li><Link className="nav-link" to="/login">Login</Link></li>
+                        <li><Link className="nav-link" to="/logout">Logout</Link></li>
                     </ul>
                 </nav>
             </header>
