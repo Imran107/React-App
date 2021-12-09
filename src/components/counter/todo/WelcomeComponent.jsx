0db212d17b4b72ba976c1d06e5f705faf00fef12
Imran107 <imran.itim@gmail.com>
2021-12-09 21:39:52 +0500

    Add axios libraries for web service calls(promise)

--- src/components/counter/todo/WelcomeComponent.jsx
+++ src/components/counter/todo/WelcomeComponent.jsx
@@ -2,6 +2,13 @@ import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
 
 export default class WelcomeComponent extends Component{
+
+    constructor(props){
+        super(props)
+
+        this.getCustomizedMessage = this.getCustomizedMessage.bind(this)
+    }
+
     render(){
         return (
             <>
@@ -9,7 +16,15 @@ export default class WelcomeComponent extends Component{
                 <div className="container"> 
                     Welcome {this.props.match.params.name}. You can manage your todos by <Link to="/todos">Clicking here</Link>
                 </div>
+                <div className="container"> 
+                   Click here to get the customized message 
+                   <button className="btn btn-success" onClick={this.getCustomizedMessage}> Get Message</button>
+                </div>
             </>
         );
+    }
+
+    getCustomizedMessage(){
+        console.log('');
     }
 }
