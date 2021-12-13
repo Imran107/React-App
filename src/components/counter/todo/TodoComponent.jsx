e32e8a229e2acbd8acd37a497bc4a48915f29f96
Imran107 <imran.itim@gmail.com>
2021-12-13 23:40:58 +0500

    Installed moment to format date and install Formik library for react forms, create form elements

--- src/components/counter/todo/TodoComponent.jsx
+++ src/components/counter/todo/TodoComponent.jsx
@@ -1,10 +1,41 @@
 import React, {Component} from 'react'
+import moment from 'moment';
+import { Formik, Form, Field } from 'formik';
 
 class TodoComponent extends Component{
+
+    constructor(props){
+        super(props)
+        this.state = {
+            id:this.props.match.params.id,
+            description:"Learning Forms",
+            targetDate : moment(new Date()).format('YYYY-MM-DD')
+        }
+    }
+
     render(){
         return(
             <div>
-                Todo Id is {this.props.match.params.id}
+                <h1>Todo</h1>
+                <div classname="container">
+                    <Formik>
+                        {
+                            (props) => (
+                                <Form>
+                                    <fieldset className="form-group">
+                                        <label>Description</label>
+                                        <Field type="text" className="form-control" name="description"></Field>
+                                    </fieldset>
+                                    <fieldset className="form-group">
+                                        <label>Target date</label>
+                                        <Field type="date" className="form-control" name="targetDate"></Field>
+                                    </fieldset>
+                                    <button type="submit" className="btn btn-success">Save</button>
+                                </Form>
+                            )
+                        }
+                    </Formik>
+                </div>
             </div>
         );
     }
