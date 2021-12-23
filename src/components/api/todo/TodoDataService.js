23ebe541694c0d158716c18f063469ccca0fe61a
Imran107 <imran.itim@gmail.com>
2021-12-23 19:10:49 +0500

    Change web service calls to interact from JPA

--- src/components/api/todo/TodoDataService.js
+++ src/components/api/todo/TodoDataService.js
@@ -1,26 +1,26 @@
 import axios from 'axios'
-import { API_URL } from '../../../Constants'
+import { API_URL, JPA_API_URL } from '../../../Constants'
 
 class TodoDataService{
 
     retreiveAllTodos(name){
-        return axios.get(`${API_URL}/users/${name}/todos`);
+        return axios.get(`${JPA_API_URL}/users/${name}/todos`);
     }
 
     retreiveTodo(name, id){
-        return axios.get(`${API_URL}/users/${name}/todos/${id}`);
+        return axios.get(`${JPA_API_URL}/users/${name}/todos/${id}`);
     }
 
     deleteTodo(username, id){
-        return axios.delete(`${API_URL}/delete/${username}/todos/${id}`);
+        return axios.delete(`${JPA_API_URL}/delete/${username}/todos/${id}`);
     }
 
     updateTodo(username, id, todo){
-        return axios.put(`${API_URL}/users/${username}/todos/${id}`, todo);
+        return axios.put(`${JPA_API_URL}/users/${username}/todos/${id}`, todo);
     }
 
     createTodo(username, todo){
-        return axios.post(`${API_URL}/users/${username}/todos/`, todo);
+        return axios.post(`${JPA_API_URL}/users/${username}/todos/`, todo);
     }
 
 }
